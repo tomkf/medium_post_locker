0bed003479fc4a266df6f94c4271743b3acf9ded
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-21 13:11:43 -0400

    classes should be fine

--- model.rb
+++ model.rb
@@ -1,28 +1,16 @@
-# Its state:
-# What do we need to store in a post to be able to serve the user stories?
-# The answer will give you the instance variables.
-
-# Its behavior:
-# What transformations will we need to perform on a post?
-# Which pieces of state will we need to expose to reading? to writing?
-# The answers will give you the public instance methods.
-
-# When you think you're done,
-# test your class in irb, fix bugs, and move on to the next class.
-
-
-# . Our program should be able to instantiate a Post with only a path.
-
-require_relative 'repo.rb'
-
 class Post
-  # will manipulate the data in our program
-  attr_reader :post_author, :post_title, :url_pathway, :content
+  attr_reader :post_author, :post_title, :url_pathway, :content, :check_mark
+  attr_writer :check_mark
 
-  def initialize(url_pathway, post_title, post_author, content)
-    @url_pathway = "https://medium.com/" + url_pathway
-    @post_title = post_title
-    @post_author = post_author
-    @content = content
+  def initialize(post_obj)
+    @url_pathway = post_obj[:url]
+    @post_title = post_obj[:title]
+    @post_author = post_obj[:post_author]
+    @content = post_obj[:content]
+    @check_mark = false
+  end
+
+  def mark_as_read
+    @check_mark = true
   end
 end
--- router.rb
+++ router.rb
@@ -1,49 +1,41 @@
 class Router
-#is init. with the controller (the controller contains the view)
-    initialize(controller)
-    @controller = controller
-    @running = true
+  # is init. with the controller (the controller contains the view)
+  initialize(controller)
+  @controller = controller
+  @running = true
     end
 
-    def run    #this command runs the app 
-        puts "------------------------------------"
-       puts "Welcome to your Medium pocket reader"
-       puts  "------------------------------------"
-    while @running
-    list_options
+def run # this command runs the app
+  puts '------------------------------------'
+  puts 'Welcome to your Medium pocket reader'
+  puts '------------------------------------'
+  list_options while @running
+
+  def list_options
+    puts '----------------------------'
+    puts 'What do you want to do next?'
+    puts '----------------------------'
+    puts "1. List posts
+    2. Save post for later
+    3. Read post
+    4. Mark post as read
+    5. Exit "
+    user_input = gets.chomp.to_i
+    if user_input == 5
+      @running = false
+      puts 'Goodbye'
     end
+    user_option(user_input)
+  end
 
-    def list_options 
-        puts "----------------------------"
-        puts "What do you want to do next?"
-        puts "----------------------------"
-        puts "1. List posts
-        2. Save post for later
-        3. Read post
-        4. Mark post as read
-        5. Exit "
-       user_input = gets.chomp.to_i
-       if user_input == 5 
-        @running = false 
-        puts "Goodbye"
-       end
-       user_option(user_input)
-    end
-
-
-    def user_option(user_input)
-        case user_input
-        when 1
-         @controller.save_post
-        when 2
-        @controller.read_post
-        when 3 
-       @controller.check_off 
-        end
-    end
-    end
-    
-
-
-    
-
+  def user_option(user_input)
+    case user_input
+    when 1
+      @controller.save_post
+    when 2
+      @controller.read_post
+    when 3
+      @controller.check_off
+      end
+  end
+end
