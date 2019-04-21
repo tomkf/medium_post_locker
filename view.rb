1e676f431aa4b6a23cf204f21896110d79c74487
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-21 13:42:34 -0400

    init

---
+++ view.rb
@@ -0,0 +1,9 @@
+class View
+
+def get_pathway
+puts "Enter the extention of the url: e.g for the url "https://medium.com/@johnzimmer/the-end-of-traffic-6d255c03207d" enter @johnzimmer/the-end-of-traffic-6d255c03207d" 
+user_input = gets.chomp 
+end
+
+
+end
