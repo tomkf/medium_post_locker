ff0b0fde07360f32218380f86ced0556887f2e6c
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-21 12:39:52 -0400

    should be fine

--- router.rb
+++ router.rb
@@ -1,23 +1,11 @@
-#this file will run the actuall app
-# router.run
-
-
-# This time we didn't give you the app.rb.
-#  Don't be afraid, just take the problem from the end.
-#  We know that the purpose of the app.rb file is to call router.run.
-#   This means you need to instantiate a router which is an instance of our Router class.
-#   OK so that's a Router.new(controller). That means we need a controller...
-#    Following this train of thoughts will lead you to the whole code.
-
-
 class Router
-
+#is init. with the controller (the controller contains the view)
     initialize(controller)
     @controller = controller
     @running = true
     end
 
-    def run 
+    def run    #this command runs the app 
         puts "------------------------------------"
        puts "Welcome to your Medium pocket reader"
        puts  "------------------------------------"
@@ -58,3 +46,4 @@ class Router
 
 
     
+
